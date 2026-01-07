0e77ab59bc02ecfe9cfd47d6061aa400f8c4adb7
mysoshake <mysoshake@gmail.com>
2026-01-08 00:49:46 +1200

    不要なimport削除

--- src/lib/core/constant.ts
+++ src/lib/core/constant.ts
@@ -1,9 +1,9 @@
 // ./src/lib/core/constant.ts
 
-import { STUDENT_MAIN_REPOSITORY_LOCAL } from "./constant.local";
+// import { STUDENT_MAIN_REPOSITORY_LOCAL } from "./constant.local";
 
 export const STUDENT_MAIN_PATH = "dist/student/student-viewer.js";
 export const STUDENT_STYLE_PATH = "src/style.css";
 
 export const STUDENT_MAIN_REPOSITORY = "https://cdn.jsdelivr.net/gh/mysoshake/RNY-WhiteBoard2@latest/";
-export const STUDENT_MAIN_REPOSITORY_NOTUSED = STUDENT_MAIN_REPOSITORY_LOCAL;
+// export const STUDENT_MAIN_REPOSITORY_NOTUSED = STUDENT_MAIN_REPOSITORY_LOCAL;
