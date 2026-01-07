74da08320c2dd9ca8ce068edc4d21f81e93650d1
mysoshake <mysoshake@gmail.com>
2026-01-08 00:31:15 +1200

    デプロイ設定001

--- vite.config.tc.ts
+++ vite.config.tc.ts
@@ -3,13 +3,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // 開発サーバーの設定
-    server: {
-        open: true, // 自動でブラウザを開く
-    },
-    build: {
-        outDir: 'dist/teacher',
-        emptyOutDir: true,
-        target: 'esnext', // 教員側は最新ブラウザ前提
-    }
+  base: '/RNY-Whiteboard2/',
+  // 開発サーバーの設定
+  server: {
+    open: true, // 自動でブラウザを開く
+  },
+  build: {
+    outDir: 'dist/teacher',
+    emptyOutDir: true,
+    target: 'esnext', // 教員側は最新ブラウザ前提
+  }
 });
