59bbe5be0621052275197877d43e229bd810e363
mysoshake <mysoshake@gmail.com>
2026-01-15 02:51:01 +1200

    COMMIT ID

--- src/lib/core/constant.ts
+++ src/lib/core/constant.ts
@@ -4,7 +4,7 @@
 export const IS_DEBUG_MODE = false;
 
 // export const STUDENT_MAIN_REPOSITORY = STUDENT_MAIN_REPOSITORY_LOCAL;
-export const STUDENT_MAIN_REPOSITORY = "https://cdn.jsdelivr.net/gh/mysoshake/RNY-WhiteBoard2@7431e67a1f903b6a2d044da8a51345c766b0de2a/";
+export const STUDENT_MAIN_REPOSITORY = "https://cdn.jsdelivr.net/gh/mysoshake/RNY-WhiteBoard2@4e78c3c753f35d71b127624247770ee7c6505698/";
 
 export const STUDENT_MAIN_PATH = "dist/student/student-viewer.js";
 export const STUDENT_STYLE_PATH = "src/style.css";
