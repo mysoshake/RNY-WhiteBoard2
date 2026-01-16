acc6b7a2c3b7da64af3fa84ed0b50833e43fd7ea
mysoshake <mysoshake@gmail.com>
2026-01-17 00:33:55 +1200

    確定

--- src/lib/core/constant.ts
+++ src/lib/core/constant.ts
@@ -1,10 +1,10 @@
 // ./src/lib/core/constant.ts
-import { STUDENT_MAIN_REPOSITORY_LOCAL } from "./constant.local";
+// import { STUDENT_MAIN_REPOSITORY_LOCAL } from "./constant.local";
 
-export const IS_DEBUG_MODE = true;
+export const IS_DEBUG_MODE = false;
 
-export const STUDENT_MAIN_REPOSITORY = STUDENT_MAIN_REPOSITORY_LOCAL;
-// export const STUDENT_MAIN_REPOSITORY = "https://cdn.jsdelivr.net/gh/mysoshake/RNY-WhiteBoard2@4e78c3c753f35d71b127624247770ee7c6505698/";
+// export const STUDENT_MAIN_REPOSITORY = STUDENT_MAIN_REPOSITORY_LOCAL;
+export const STUDENT_MAIN_REPOSITORY = "https://cdn.jsdelivr.net/gh/mysoshake/RNY-WhiteBoard2@4e78c3c753f35d71b127624247770ee7c6505698/";
 
 export const STUDENT_MAIN_PATH = "dist/student/student-viewer.js";
 export const STUDENT_STYLE_PATH = "src/style.css";
