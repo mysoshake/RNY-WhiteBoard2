5b3bd820a1d127c72e57f637ec5d6e680e460eac
mysoshake <mysoshake@gmail.com>
2026-01-22 05:44:18 +1200

    STUDENT_MAIN_REPOSITORYのURLを更新

--- src/lib/core/constant.ts
+++ src/lib/core/constant.ts
@@ -4,7 +4,7 @@
 export const IS_DEBUG_MODE = false;
 
 // export const STUDENT_MAIN_REPOSITORY = STUDENT_MAIN_REPOSITORY_LOCAL;
-export const STUDENT_MAIN_REPOSITORY = "https://cdn.jsdelivr.net/gh/mysoshake/RNY-WhiteBoard2@851e3660183022412636977ce46ffe988fb5f7cb/";
+export const STUDENT_MAIN_REPOSITORY = "https://cdn.jsdelivr.net/gh/mysoshake/RNY-WhiteBoard2@90d7bc227d26cc5148d667f85ded8d8871cc39cb/";
 
 export const STUDENT_MAIN_PATH = "dist/student/student-viewer.js";
 export const STUDENT_STYLE_PATH = "src/style.css";
